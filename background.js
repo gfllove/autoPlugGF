82321cd5158a5d551b850dee40fbc07081970239
gfl <gfl@163.com>
2017-07-14 18:04:23 +0800

    添加2个新文件和修改代码

--- background.js
+++ background.js
@@ -1,6 +1,22 @@
-/**
- * 
- */
 
 
 
+
+
+
+chrome.webRequest.onBeforeSendHeaders.addListener(function(details) {
+	
+	//从所有的request中删除User-Agent的header
+	for (var i = 0; i < details.requestHeaders.length; ++i) {
+		if (details.requestHeaders[i].name === 'User-Agent') {
+			details.requestHeaders.splice(i, 1,'linden_test');
+			break;
+		}
+	}
+	console.log("background8888888888888888");
+	return {
+		requestHeaders : details.requestHeaders
+	};
+}, {
+	urls : [ "<all_urls>" ]
+}, ["blocking","requestHeaders"]);
